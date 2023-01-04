fdb004e5097a52505fc65c19bc9dc28965e388fe
Felipe <felipe21fab@gmail.com>
2023-01-04 16:13:19 -0300

    Updated showbooks function to show as a grid

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
-const body = document.querySelector('body')
-const bookGrid = document.createElement('div')
 
-body.appendChild(bookGrid)
+const bookGrid = document.getElementById('bookGrid')
+
 
 
 let myLibrary = []
@@ -11,6 +10,9 @@ function Book(title, author, pages, is_read){
 	this.author = author
 	this.pages = pages
 	this.is_read = is_read
+
+
+
 	this.info = function() {
 		let read = ''
 		if (this.is_read === true) {
@@ -25,6 +27,7 @@ function Book(title, author, pages, is_read){
 
 function addBookToLibrary(book) {
     if (book instanceof (Book)) {
+        
         myLibrary.push(book)
     }
 }
@@ -32,12 +35,25 @@ function addBookToLibrary(book) {
 function showBooks(){
     for(let i = 0; i <= myLibrary.length-1; i++){
         console.log((myLibrary[i]).info())
-        bookGrid.textContent += (myLibrary[i]).info()
+        const book = document.createElement('div')
+        book.classList.add('card')
+        book.textContent = (myLibrary[i]).info()
+        bookGrid.appendChild(book)
     }
 }
 
 let book1 = new Book('The Hobbit', 'J.R.R. Tolkien', 295, false)
+let book2 = new Book('The Hobbit', 'J.R.R. Tolkien', 295, false)
+let book3 = new Book('The Hobbit', 'J.R.R. Tolkien', 295, false)
+let book4 = new Book('The Hobbit', 'J.R.R. Tolkien', 295, false)
+let book5 = new Book('The Hobbit', 'J.R.R. Tolkien', 295, false)
+let book6 = new Book('The Hobbit', 'J.R.R. Tolkien', 295, false)
 
 addBookToLibrary(book1)
+addBookToLibrary(book2)
+addBookToLibrary(book3)
+addBookToLibrary(book4)
+addBookToLibrary(book5)
+addBookToLibrary(book6)
 
 showBooks()
